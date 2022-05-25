40121d80c47e737ae0c3262495d8b377ffd22819
Marlena Tsang <Marlena345@gmail.com>
2022-05-25 10:51:01 -0400

    Completed assignment

--- src/features/books/booksSlice.js
+++ src/features/books/booksSlice.js
@@ -1,18 +1,4 @@
 // Action Creators
-export const addAuthor = (author) => {
-  return {
-    type: "authors/add",
-    payload: author,
-  };
-};
-
-export const removeAuthor = (id) => {
-  return {
-    type: "authors/remove",
-    payload: id,
-  };
-};
-
 export const addBook = (book) => {
   return {
     type: "books/add",
@@ -28,12 +14,9 @@ export const removeBook = (id) => {
 };
 
 // Reducers
-const initialState = {
-  authors: [], //array of authors
-  books: [], // array of books
-};
+const initialState = [];
 
-export default function reducer(state = initialState, action) {
+export default function booksReducer(state = initialState, action) {
   switch (action.type) {
     case "books/add":
       return {
@@ -47,22 +30,6 @@ export default function reducer(state = initialState, action) {
         ...state,
         books: newBooks,
       };
-
-    case "authors/add":
-      return {
-        ...state,
-        authors: [...state.authors, action.payload],
-      };
-
-    case "authors/remove":
-      const newAuthors = state.authors.filter(
-        (author) => author.id !== action.payload
-      );
-      return {
-        ...state,
-        authors: newAuthors,
-      };
-
     default:
       return state;
   }
